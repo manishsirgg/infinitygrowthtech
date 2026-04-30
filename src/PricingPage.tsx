88051eec51fddc28943cedb142c948b68e62db6f
manishsirgg <manishsirgg@gmail.com>
2026-04-30 11:46:20 +0530

    Fix missing footer on pricing page

--- src/PricingPage.tsx
+++ src/PricingPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import WhatsAppFloat from './components/WhatsAppFloat';
+import Footer from './components/Footer';
 
 type PricingCardProps = {
   title: string;
@@ -171,6 +172,7 @@ const PricingPage: React.FC = () => {
           </div>
         </section>
       </main>
+      <Footer />
       <WhatsAppFloat />
     </div>
   );
